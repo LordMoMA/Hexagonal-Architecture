234943d1e782d5f483eb579630aa17a15a85148d
David Lee <lihongyi0512@gmail.com>
2023-05-03 03:28:04 +0800

    hexa-arch

--- go.mod
+++ go.mod
@@ -2,4 +2,12 @@ module github.com/LordMoMA/Hexagonal-Architecture
 
 go 1.20
 
-require github.com/google/uuid v1.3.0
+require (
+	github.com/google/uuid v1.3.0
+	github.com/jinzhu/gorm v1.9.16
+)
+
+require (
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/lib/pq v1.1.1 // indirect
+)
---
+++ internal/adapters/handler/http.go
@@ -0,0 +1,68 @@
+package handler
+
+import (
+	"net/http"
+
+	"github.com/LordMoMA/Hexagonal-Architecture/internal/core/domain"
+	"github.com/LordMoMA/Hexagonal-Architecture/internal/core/services"
+	"github.com/gin-gonic/gin"
+)
+
+type HTTPHandler struct {
+   svc services.MessengerService
+}
+
+func NewHTTPHandler(MessengerService services.MessengerService) *HTTPHandler {
+   return &HTTPHandler{
+       svc: MessengerService,
+   }
+}
+
+func (h *HTTPHandler) SaveMessage(ctx *gin.Context) {
+   var message domain.Message
+   if err := ctx.ShouldBindJSON(&message); err != nil {
+       ctx.JSON(http.StatusBadRequest, gin.H{
+           "Error": err,
+       })
+
+       return
+   }
+
+   err := h.svc.SaveMessage(message)
+   if err != nil {
+       ctx.JSON(http.StatusBadRequest, gin.H{
+           "error": err,
+       })
+       return
+   }
+
+   ctx.JSON(http.StatusCreated, gin.H{
+       "message": "New message created successfully",
+   })
+}
+
+func (h *HTTPHandler) ReadMessage(ctx *gin.Context) {
+   id := ctx.Param("id")
+   message, err := h.svc.ReadMessage(id)
+
+   if err != nil {
+       ctx.JSON(http.StatusBadRequest, gin.H{
+           "error": err.Error(),
+       })
+       return
+   }
+   ctx.JSON(http.StatusOK, message)
+}
+
+func (h *HTTPHandler) ReadMessages(ctx *gin.Context) {
+
+   messages, err := h.svc.ReadMessages()
+
+   if err != nil {
+       ctx.JSON(http.StatusBadRequest, gin.H{
+           "error": err.Error(),
+       })
+       return
+   }
+   ctx.JSON(http.StatusOK, messages)
+}
