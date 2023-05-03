72705db8f6aee0d5dc78b10a32a023b37337f6a1
David Lee <lihongyi0512@gmail.com>
2023-05-03 20:11:12 +0800

    “gitignore”

--- git_automation.py
+++
@@ -1,8 +0,0 @@
-import os
-import time
-
-while True:
-    os.system('git add .')
-    os.system('git commit -m "hexa-arch"')
-    os.system('git push -u origin main')
-    time.sleep(300)  # sleep for 5 min
