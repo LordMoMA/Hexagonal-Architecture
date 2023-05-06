b7a88b26e0e90ea86789a494a363f7aa1fff1933
David Lee <lihongyi0512@gmail.com>
2023-05-06 22:53:18 +0800

    crud

--- internal/core/services/user.go
+++ internal/core/services/user.go
@@ -1,7 +1,21 @@
 package services
 
-import "github.com/LordMoMA/Hexagonal-Architecture/internal/core/ports"
+import (
+	"github.com/LordMoMA/Hexagonal-Architecture/internal/core/domain"
+	"github.com/LordMoMA/Hexagonal-Architecture/internal/core/ports"
+)
 
 type UserService struct {
 	repo ports.UserRepository
 }
+
+func NewUserService(repo ports.UserRepository) *UserService {
+	return &UserService{
+		repo: repo,
+	}
+}
+
+func (u *UserService) CreateUser(user domain.User) error {
+	return u.repo.CreateUser(user)
+}
+
