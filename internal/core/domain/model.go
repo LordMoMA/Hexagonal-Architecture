e08f66d75893e2175f47d0fd93d2db73ff666884
David Lee <lihongyi0512@gmail.com>
2023-05-06 19:52:35 +0800

    golang-code

--- internal/core/domain/model.go
+++ internal/core/domain/model.go
@@ -3,3 +3,10 @@ type Message struct {
    ID   string `json:"id"`
    Body string `json:"body"`
 }
+
+type User struct {
+   ID   int `json:"id"`
+   Email    string `json:"email"`
+	Password string `json:"password"`
+	Membership bool `json:"is_member"`
+}
