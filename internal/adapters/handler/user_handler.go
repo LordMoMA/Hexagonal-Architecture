9af31e9e6a00164ff4b961c8fb9b9f295a587b5c
David Lee <lihongyi0512@gmail.com>
2023-05-07 03:17:54 +0800

    crud

--- internal/adapters/handler/user_handler.go
+++ internal/adapters/handler/user_handler.go
@@ -1,6 +1,7 @@
 package handler
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
 	"time"
@@ -71,79 +72,156 @@ func (h *UserHandler) ReadUsers(ctx *gin.Context) {
 }
 
 func (h *UserHandler) UpdateUser(ctx *gin.Context) {
-	apiCfg, err := repository.LoadAPIConfig()
+	token, err := getToken(ctx)
 	if err != nil {
-		ctx.JSON(http.StatusBadRequest, gin.H{
-			"error": err,
-		})
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
 	}
 
+	if err := validateToken(token); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
 
+	id := ctx.Param("id")
+	user, err := getUserFromBody(ctx)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	if err := h.svc.UpdateUser(id, user.Email, user.Password); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, gin.H{"message": "User updated successfully"})
+}
+
+func getToken(ctx *gin.Context) (string, error) {
 	authHeader := ctx.Request.Header.Get("Authorization")
 	if authHeader == "" {
-		ctx.JSON(http.StatusBadRequest, gin.H{
-			"error": "Token not found",
-		})
+		return "", errors.New("Token not found")
 	}
 	tokenString := authHeader[7:]
+	return tokenString, nil
+}
 
-	// parse and validate the token
-	token, err := jwt.ParseWithClaims(tokenString, &jwt.RegisteredClaims{}, func(token *jwt.Token) (interface{},error) {
+func validateToken(tokenString string) error {
+	apiCfg, err := repository.LoadAPIConfig()
+	if err != nil {
+		return err
+	}
+
+	token, err := jwt.ParseWithClaims(tokenString, &jwt.RegisteredClaims{}, func(token *jwt.Token) (interface{}, error) {
 		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
 			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
 		}
 		return []byte(apiCfg.JWTSecret), nil
 	})
-
 	if err != nil {
-		ctx.JSON(http.StatusBadRequest, gin.H{
-			"error": err,
-		})
+		return err
 	}
 
 	if !token.Valid {
-		ctx.JSON(http.StatusBadRequest, gin.H{
-			"error": "Token not valid",
-		})
+		return errors.New("Token not valid")
 	}
 
-	// check token has expired or not
 	claims, ok := token.Claims.(*jwt.RegisteredClaims)
 	if !ok || claims.ExpiresAt == nil || claims.ExpiresAt.Before(time.Now().UTC()) {
-		ctx.JSON(http.StatusBadRequest, gin.H{
-			"error": "Token has expired",
-		})
+		return errors.New("Token has expired")
 	}
 
-	// check if token is a refresh token
 	if claims.Issuer == "LordMoMA-refresh" {
-		ctx.JSON(http.StatusBadRequest, gin.H{
-			"error": "Token is a refresh token, please use access token.",
-		})
+		return errors.New("Token is a refresh token, please use access token")
 	}
 
-	id := ctx.Param("id")
+	return nil
+}
+
+func getUserFromBody(ctx *gin.Context) (domain.User, error) {
 	var user domain.User
 	if err := ctx.ShouldBindJSON(&user); err != nil {
-		ctx.JSON(http.StatusBadRequest, gin.H{
-			"Error": err,
-		})
-
-		return
+		return domain.User{}, err
 	}
-
-	err = h.svc.UpdateUser(id, user.Email, user.Password)
-	if err != nil {
-		ctx.JSON(http.StatusBadRequest, gin.H{
-			"error": err,
-		})
-		return
-	}
-
-	ctx.JSON(http.StatusOK, gin.H{
-		"message": "User updated successfully",
-	})
+	return user, nil
 }
+
+
+// func (h *UserHandler) UpdateUser(ctx *gin.Context) {
+// 	apiCfg, err := repository.LoadAPIConfig()
+// 	if err != nil {
+// 		ctx.JSON(http.StatusBadRequest, gin.H{
+// 			"error": err,
+// 		})
+// 	}
+
+
+// 	authHeader := ctx.Request.Header.Get("Authorization")
+// 	if authHeader == "" {
+// 		ctx.JSON(http.StatusBadRequest, gin.H{
+// 			"error": "Token not found",
+// 		})
+// 	}
+// 	tokenString := authHeader[7:]
+
+// 	// parse and validate the token
+// 	token, err := jwt.ParseWithClaims(tokenString, &jwt.RegisteredClaims{}, func(token *jwt.Token) (interface{},error) {
+// 		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+// 			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+// 		}
+// 		return []byte(apiCfg.JWTSecret), nil
+// 	})
+
+// 	if err != nil {
+// 		ctx.JSON(http.StatusBadRequest, gin.H{
+// 			"error": err,
+// 		})
+// 	}
+
+// 	if !token.Valid {
+// 		ctx.JSON(http.StatusBadRequest, gin.H{
+// 			"error": "Token not valid",
+// 		})
+// 	}
+
+// 	// check token has expired or not
+// 	claims, ok := token.Claims.(*jwt.RegisteredClaims)
+// 	if !ok || claims.ExpiresAt == nil || claims.ExpiresAt.Before(time.Now().UTC()) {
+// 		ctx.JSON(http.StatusBadRequest, gin.H{
+// 			"error": "Token has expired",
+// 		})
+// 	}
+
+// 	// check if token is a refresh token
+// 	if claims.Issuer == "LordMoMA-refresh" {
+// 		ctx.JSON(http.StatusBadRequest, gin.H{
+// 			"error": "Token is a refresh token, please use access token.",
+// 		})
+// 	}
+
+// 	id := ctx.Param("id")
+// 	var user domain.User
+// 	if err := ctx.ShouldBindJSON(&user); err != nil {
+// 		ctx.JSON(http.StatusBadRequest, gin.H{
+// 			"Error": err,
+// 		})
+
+// 		return
+// 	}
+
+// 	err = h.svc.UpdateUser(id, user.Email, user.Password)
+// 	if err != nil {
+// 		ctx.JSON(http.StatusBadRequest, gin.H{
+// 			"error": err,
+// 		})
+// 		return
+// 	}
+
+// 	ctx.JSON(http.StatusOK, gin.H{
+// 		"message": "User updated successfully",
+// 	})
+// }
 
 
 func (h *UserHandler) DeleteUser(ctx *gin.Context) {
