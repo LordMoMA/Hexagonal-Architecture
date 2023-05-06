a555c9ecdccfe2a2ae6492b74e81db83448f7e27
David Lee <lihongyi0512@gmail.com>
2023-05-07 00:06:25 +0800

    crud

--- internal/adapters/repository/db.go
+++ internal/adapters/repository/db.go
@@ -8,11 +8,6 @@ type DB struct {
 	db *gorm.DB
 }
 
-// type DBStructure struct {
-// 	Users map[int]domain.User `json:"users"`
-// 	Messages map[int]domain.Message `json:"messages"`
-// }
-
 // new database
 func NewDB(db *gorm.DB) *DB {
 	return &DB{
