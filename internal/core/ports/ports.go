b4fbd192dadd06062a9297e7f6928b38b0515033
David Lee <lihongyi0512@gmail.com>
2023-05-07 00:56:25 +0800

    crud

--- internal/core/ports/ports.go
+++ internal/core/ports/ports.go
@@ -24,7 +24,7 @@ type UserRepository interface {
    CreateUser(email, password string) (*domain.User, error)
    ReadUser(id string) (*domain.User, error)
    ReadUsers() ([]*domain.User, error)
-   UpdateUser(id string, user domain.User) error
+   UpdateUser(id, email, password string) error
    DeleteUser(id string) error
 }
 
