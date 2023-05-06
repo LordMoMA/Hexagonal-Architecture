1fe705587a9b5790c9f2b3f60b316da5e49418d8
David Lee <lihongyi0512@gmail.com>
2023-05-06 23:23:44 +0800

    crud

--- internal/adapters/handler/user_handler.go
+++ internal/adapters/handler/user_handler.go
@@ -1,6 +1,12 @@
 package handler
 
-import "github.com/LordMoMA/Hexagonal-Architecture/internal/core/services"
+import (
+	"net/http"
+
+	"github.com/LordMoMA/Hexagonal-Architecture/internal/core/domain"
+	"github.com/LordMoMA/Hexagonal-Architecture/internal/core/services"
+	"github.com/gin-gonic/gin"
+)
 
 type UserHandler struct {
 	svc services.UserService
@@ -10,4 +16,28 @@ func NewUserHandler(UserService services.UserService) *UserHandler {
 	return &UserHandler{
 		svc: UserService,
 	}
+}
+
+func (h *UserHandler) CreateUser(ctx *gin.Context) {
+	var user domain.User
+	if err := ctx.ShouldBindJSON(&user); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"Error": err,
+		})
+
+		return
+	}
+
+	err := h.svc.CreateUser(user)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"error": err,
+		})
+		return
+	}
+
+	ctx.JSON(http.StatusCreated, gin.H{
+		"message": "New user created successfully",
+	})	
+	
 }
