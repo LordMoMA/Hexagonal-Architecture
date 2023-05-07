0e75782c360d8168857b6f7f58c094838c96068f
David Lee <lihongyi0512@gmail.com>
2023-05-08 02:35:44 +0800

    auth

--- internal/core/domain/model.go
+++ internal/core/domain/model.go
@@ -1,16 +1,22 @@
 package domain
 
 type Message struct {
-   ID   string `json:"id"`
-   UserID string `json:"user_id"`
-   Body string `json:"body"`
+	ID     string `json:"id"`
+	UserID string `json:"user_id"`
+	Body   string `json:"body"`
 }
 
 type User struct {
-   ID       string `json:"id"`
-   Email    string `json:"email"`
-	Password string `json:"password"`
-	Membership bool `json:"is_member"`
+	ID         string `json:"id"`
+	Email      string `json:"email"`
+	Password   string `json:"password"`
+	Membership bool   `json:"is_member"`
 }
 
-
+type Payment struct {
+	OrderID  string  `json:"id"`
+	UserID   string  `json:"user_id"`
+	Amount   float64 `json:"amount"`
+	Currency string  `json:"currency"`
+	Status   string  `json:"status"`
+}
