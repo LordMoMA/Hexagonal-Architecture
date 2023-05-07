fafa207985155a95fb0e91c3dd6accfe58be003a
David Lee <lihongyi0512@gmail.com>
2023-05-07 22:41:34 +0800

    auth

---
+++ internal/adapters/handler/webhook_handler.go
@@ -0,0 +1,44 @@
+package handler
+
+import (
+	"errors"
+	"net/http"
+
+	"github.com/LordMoMA/Hexagonal-Architecture/internal/adapters/repository"
+	"github.com/LordMoMA/Hexagonal-Architecture/internal/core/domain"
+	"github.com/gin-gonic/gin"
+)
+
+func (h *UserHandler) UpdateMembershipStatus(ctx *gin.Context) {
+	apiCfg, err := repository.LoadAPIConfig()
+	if err != nil {
+		HandleError(ctx, http.StatusBadRequest, err)
+		return
+	}
+
+	// get api key from config
+	apiKey := apiCfg.APIKey
+
+	// check if api key is valid
+	if ctx.Request.Header.Get("Authorization") != apiKey {
+		HandleError(ctx, http.StatusBadRequest, errors.New("invalid api key"))
+		return
+	}
+
+
+	var user domain.User
+	if err := ctx.ShouldBindJSON(&user); err != nil {
+		HandleError(ctx, http.StatusBadRequest, err)
+		return
+	}
+	
+	err = h.svc.UpdateMembershipStatus(user.ID, true)
+	if err != nil {
+		HandleError(ctx, http.StatusBadRequest, err)
+		return
+	}
+
+	ctx.JSON(http.StatusOK, gin.H{
+		"message": "User's membership status updated successfully",
+	})
+}
