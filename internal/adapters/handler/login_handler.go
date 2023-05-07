bfe59afe503730d8b8100ce60046f5317c447dbb
David Lee <lihongyi0512@gmail.com>
2023-05-07 22:02:06 +0800

    auth

---
+++ internal/adapters/handler/login_handler.go
@@ -0,0 +1,30 @@
+package handler
+
+import (
+	"net/http"
+
+	"github.com/LordMoMA/Hexagonal-Architecture/internal/core/domain"
+	"github.com/gin-gonic/gin"
+)
+
+func (h *UserHandler) LoginUser(ctx *gin.Context) {
+	var user domain.User
+	if err := ctx.ShouldBindJSON(&user); err != nil {
+		HandleError(ctx, http.StatusBadRequest, err)
+		return
+	}
+
+	response, err := h.svc.LoginUser(user.Email, user.Password)
+	if err != nil {
+		HandleError(ctx, http.StatusBadRequest, err)
+		return
+	}
+
+	ctx.JSON(http.StatusOK, gin.H{
+	"id":             response.ID,
+    "email":          response.Email,
+    "access_token":   response.AccessToken,
+    "refresh_token":  response.RefreshToken,
+    "is_member":      response.Membership,
+	})
+}
--- internal/adapters/handler/user_handler.go
+++ internal/adapters/handler/user_handler.go
@@ -159,25 +159,3 @@ func ValidateToken(authHeader string, jwtSecret string) (string, error) {
 
 	return userID, nil
 }
-
-func (h *UserHandler) LoginUser(ctx *gin.Context) {
-	var user domain.User
-	if err := ctx.ShouldBindJSON(&user); err != nil {
-		HandleError(ctx, http.StatusBadRequest, err)
-		return
-	}
-
-	response, err := h.svc.LoginUser(user.Email, user.Password)
-	if err != nil {
-		HandleError(ctx, http.StatusBadRequest, err)
-		return
-	}
-
-	ctx.JSON(http.StatusOK, gin.H{
-	"id":             response.ID,
-    "email":          response.Email,
-    "access_token":   response.AccessToken,
-    "refresh_token":  response.RefreshToken,
-    "is_member":      response.Membership,
-	})
-}
