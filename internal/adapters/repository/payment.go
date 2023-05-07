e25f5407ce2323067e497c7a2757ab21fc5fe695
David Lee <lihongyi0512@gmail.com>
2023-05-08 02:37:49 +0800

    auth

---
+++ internal/adapters/repository/payment.go
@@ -0,0 +1,5 @@
+package repository
+
+type PaymentService struct {
+	repo ports.PaymentRepository
+}
--- internal/core/ports/ports.go
+++ internal/core/ports/ports.go
@@ -5,42 +5,46 @@ import (
 	"github.com/LordMoMA/Hexagonal-Architecture/internal/core/domain"
 )
 
-
 type MessengerService interface {
-   CreateMessage(userID string, message domain.Message) error
-   ReadMessage(id string) (*domain.Message, error)
-   ReadMessages() ([]*domain.Message, error)
-   UpdateMessage(id string, message domain.Message) error
-   DeleteMessage(id string) error
+	CreateMessage(userID string, message domain.Message) error
+	ReadMessage(id string) (*domain.Message, error)
+	ReadMessages() ([]*domain.Message, error)
+	UpdateMessage(id string, message domain.Message) error
+	DeleteMessage(id string) error
 }
 
-
 type MessengerRepository interface {
-   CreateMessage(userID string, message domain.Message) error
-   ReadMessage(id string) (*domain.Message, error)
-   ReadMessages() ([]*domain.Message, error)
-   UpdateMessage(id string, message domain.Message) error
-   DeleteMessage(id string) error
+	CreateMessage(userID string, message domain.Message) error
+	ReadMessage(id string) (*domain.Message, error)
+	ReadMessages() ([]*domain.Message, error)
+	UpdateMessage(id string, message domain.Message) error
+	DeleteMessage(id string) error
 }
 
 type UserService interface {
-   CreateUser(email, password string) (*domain.User,error)
-   ReadUser(id string) (*domain.User, error)
-   ReadUsers() ([]*domain.User, error)
-   UpdateUser(id, email, password string) error
-   DeleteUser(id string) error
-   LoginUser(email, password string) (*repository.LoginResponse, error)
-   UpdateMembershipStatus(id string, status bool) error
+	CreateUser(email, password string) (*domain.User, error)
+	ReadUser(id string) (*domain.User, error)
+	ReadUsers() ([]*domain.User, error)
+	UpdateUser(id, email, password string) error
+	DeleteUser(id string) error
+	LoginUser(email, password string) (*repository.LoginResponse, error)
+	UpdateMembershipStatus(id string, status bool) error
 }
 
 type UserRepository interface {
-   CreateUser(email, password string) (*domain.User, error)
-   ReadUser(id string) (*domain.User, error)
-   ReadUsers() ([]*domain.User, error)
-   UpdateUser(id, email, password string) error
-   DeleteUser(id string) error
-   LoginUser(email, password string) (*repository.LoginResponse, error)
-   UpdateMembershipStatus(id string, status bool) error
+	CreateUser(email, password string) (*domain.User, error)
+	ReadUser(id string) (*domain.User, error)
+	ReadUsers() ([]*domain.User, error)
+	UpdateUser(id, email, password string) error
+	DeleteUser(id string) error
+	LoginUser(email, password string) (*repository.LoginResponse, error)
+	UpdateMembershipStatus(id string, status bool) error
 }
 
+type UserMembershipService interface {
+	UpdateMembershipStatus(id string, status bool) error
+}
 
+type UserMembershipRepository interface {
+	UpdateMembershipStatus(id string, status bool) error
+}
