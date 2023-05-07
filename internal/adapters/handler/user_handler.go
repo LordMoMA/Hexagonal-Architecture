1f2cabf808d30298b8ca6438cd4a7a2474c0a6a1
David Lee <lihongyi0512@gmail.com>
2023-05-07 21:59:55 +0800

    auth

--- internal/adapters/handler/user_handler.go
+++ internal/adapters/handler/user_handler.go
@@ -1,12 +1,16 @@
 package handler
 
 import (
+	"errors"
+	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/LordMoMA/Hexagonal-Architecture/internal/adapters/repository"
 	"github.com/LordMoMA/Hexagonal-Architecture/internal/core/domain"
 	"github.com/LordMoMA/Hexagonal-Architecture/internal/core/services"
 	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
 )
 
 type UserHandler struct {
@@ -113,6 +117,47 @@ func (h *UserHandler) DeleteUser(ctx *gin.Context) {
 	ctx.JSON(http.StatusOK, gin.H{
 		"message": "User deleted successfully",
 	})
+}
+
+func ValidateToken(authHeader string, jwtSecret string) (string, error) {
+	// Check if token exists in the header
+	if authHeader == "" {
+		return "", errors.New("token not found")
+	}
+	
+	// Extract token from header
+	tokenString := authHeader[7:]
+
+	// Parse and validate token
+	token, err := jwt.ParseWithClaims(tokenString, &jwt.RegisteredClaims{}, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+		return []byte(jwtSecret), nil
+	})
+	if err != nil {
+		return "", err
+	}
+
+	if !token.Valid {
+		return "", errors.New("token not valid")
+	}
+
+	// Check if token has expired
+	claims, ok := token.Claims.(*jwt.RegisteredClaims)
+	if !ok || claims.ExpiresAt == nil || claims.ExpiresAt.Before(time.Now().UTC()) {
+		return "", errors.New("token has expired")
+	}
+
+	// Check if token is a refresh token
+	if claims.Issuer == "LordMoMA-refresh" {
+		return "", errors.New("token is a refresh token, please use access token")
+	}
+
+	// Extract user ID from token
+	userID := claims.Subject
+
+	return userID, nil
 }
 
 func (h *UserHandler) LoginUser(ctx *gin.Context) {
