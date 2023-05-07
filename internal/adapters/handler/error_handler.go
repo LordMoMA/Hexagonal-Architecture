093c59eea91ad8d0c31fee2041b43432fc8d673d
David Lee <lihongyi0512@gmail.com>
2023-05-07 17:00:26 +0800

    crud

--- internal/adapters/handler/error_handler.go
+++ internal/adapters/handler/error_handler.go
@@ -2,7 +2,7 @@ package handler
 
 import "github.com/gin-gonic/gin"
 
-func handleError(ctx *gin.Context, statusCode int, err error) {
+func HandleError(ctx *gin.Context, statusCode int, err error) {
 	ctx.JSON(statusCode, gin.H{
 		"error": err.Error(),
 	})
--- internal/adapters/handler/message_handler.go
+++ internal/adapters/handler/message_handler.go
@@ -147,9 +147,7 @@ func (h *MessageHandler) UpdateMessage(ctx *gin.Context) {
 func (h *MessageHandler) DeleteMessage(ctx *gin.Context) {
     apiCfg, err := repository.LoadAPIConfig()
 	if err != nil {
-		ctx.JSON(http.StatusBadRequest, gin.H{
-			"error": err,
-		})
+		HandleError(ctx, http.StatusBadRequest, err)
 		return
 	}
 
