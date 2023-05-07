78883d7f94963c88432bef08d66b4326152e4571
David Lee <lihongyi0512@gmail.com>
2023-05-08 02:39:55 +0800

    auth

--- internal/core/services/payment.go
+++ internal/core/services/payment.go
@@ -1,1 +1,11 @@
 package services
+
+type PaymentService struct {
+	repo ports.PaymentRepository
+}
+
+func NewPaymentService(repo ports.PaymentRepository) *PaymentService {
+	return &PaymentService{
+		repo: repo,
+	}
+}
