f0c15ebf401bd269f69cc2efbcfc3eb5e0487128
David Lee <lihongyi0512@gmail.com>
2023-05-08 22:23:29 +0800

    stripe

---
+++ internal/web/CheckoutForm.js
@@ -0,0 +1,12 @@
+import { PaymentElement } from "@stripe/react-stripe-js";
+
+const CheckoutForm = () => {
+  return (
+    <form>
+      <PaymentElement />
+      <button>Submit</button>
+    </form>
+  );
+};
+
+export default CheckoutForm;
