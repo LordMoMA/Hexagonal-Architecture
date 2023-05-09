957c184478cc45f3dc89b02256a9587394e3cc00
David Lee <lihongyi0512@gmail.com>
2023-05-10 00:01:06 +0800

    redis

--- internal/adapters/repository/payment.go
+++ internal/adapters/repository/payment.go
@@ -12,6 +12,22 @@ func (p *DB) Deposit(userID string, amount string, payment domain.Payment) error
 		OrderID: payment.OrderID,
 		UserID:  userID,
 		Amount:  payment.Amount,
+		Status: "success",
+	}
+	req := p.db.Create(&payment)
+	if req.RowsAffected == 0 {
+		return req.Error
+	}
+	return nil
+}
+// withdraw
+func (p *DB) Withdraw(userID string, amount string, payment domain.Payment) error {
+	payment.OrderID = uuid.New().String()
+	payment = domain.Payment{
+		OrderID: payment.OrderID,	
+		UserID:  userID,
+		Amount:  payment.Amount,
+		Status: "success",
 	}
 	req := p.db.Create(&payment)
 	if req.RowsAffected == 0 {
