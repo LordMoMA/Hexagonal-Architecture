258a0d9ff9fab2fbf1fdce8afd69b4d324d5d277
David Lee <lihongyi0512@gmail.com>
2023-05-11 01:26:58 +0800

    structure

--- internal/core/ports/cache.go
+++ internal/core/ports/cache.go
@@ -4,6 +4,6 @@ import "time"
 
 type CacheRepository interface {
 	Set(key string, value interface{}, expiration time.Duration) error
-	Get(key string, dest interface{}) error
+	Get(key string, value interface{}) error
 	Delete(key string) error
 }
